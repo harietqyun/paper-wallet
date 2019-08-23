60704c98c1dd5102b9974b79a73c7030df0d73dd
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-08-23 14:30:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var btc = require('./coins/btc.js');
 var bch = require('./coins/bch.js');
 var eth = require('./coins/eth.js');
 var eos = require('./coins/eos.js');
+var sct = require('./coins/sct.js');
 
 async function main() {
   var symbol = process.argv[2];
@@ -19,7 +20,7 @@ async function main() {
   var seed = crypto.randomBytes(32);
   
   var wallet;
-  if (['btc','btg','dash','dcr','doge','ltc','mona','qtum','rdd','zec'].indexOf(symbol) >= 0) {
+  if (['btc','btg','dash','dcr','doge','ltc','mona','qtum','rdd','sct','zec'].indexOf(symbol) >= 0) {
     wallet = btc.generateWallet(seed, symbol);
   } else if (['bch'].indexOf(symbol) >= 0) {
     wallet = bch.generateWallet(seed, symbol);
@@ -27,8 +28,10 @@ async function main() {
     wallet = eth.generateWallet(seed, symbol);
   } else if (['eos'].indexOf(symbol) >= 0) {
     wallet = eos.generateWallet(seed, symbol);
+  } else if (['sct'].indexOf(symbol) >= 0) {
+    wallet = sct.generateWallet(seed, symbol);
   } else {
-    console.log('Usage: paper-wallet [btc|bch|btg|dash|dcr|doge|eos|eth|etc|ltc|mona|qtum|rdd|zec]');
+    console.log('Usage: paper-wallet [btc|bch|btg|dash|dcr|doge|eos|eth|etc|ltc|mona|qtum|rdd|sct|zec]');
     return;
   }
 
